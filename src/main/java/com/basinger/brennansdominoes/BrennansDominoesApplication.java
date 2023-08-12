8e0516c1ff2a2770c6f4ec5b79ab42f3fa474d11
Brennan Basinger <brennan.d.basinger@gmail.com>
2023-08-12 15:41:49 -0500

    First Commit

---
+++ src/main/java/com/basinger/brennansdominoes/BrennansDominoesApplication.java
@@ -0,0 +1,13 @@
+package com.basinger.brennansdominoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrennansDominoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrennansDominoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basinger/brennansdominoes/BrennansDominoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basinger.brennansdominoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrennansDominoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
