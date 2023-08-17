3e01e83075dc4ccd0e042270fc1a878d201c1a32
Brennan Basinger <brennan.d.basinger@gmail.com>
2023-08-16 23:02:31 -0500

    Starting point before branching and adding features

--- src/main/java/com/basinger/brennansdominoes/services/GameService.java
+++ src/main/java/com/basinger/brennansdominoes/services/GameService.java
@@ -3,7 +3,6 @@ package com.basinger.brennansdominoes.services;
 import com.basinger.brennansdominoes.Position;
 import com.basinger.brennansdominoes.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -17,22 +16,18 @@ public class GameService {
 
     public Game startNewGame() {
 
+        //Create the game object
         Game game = new Game(new Player("1","PLAYER 1"), new Player("1","PLAYER 2"));
-
+        //Create the deck and shuffle
         List<Domino> shuffledDominoes = shuffleDominoes();
-
         // Distribute dominoes to players
         game.getPlayer1().setHand(new LinkedList<>(shuffledDominoes.subList(0, 7)));
         game.getPlayer2().setHand(new LinkedList<>(shuffledDominoes.subList(7, 14)));
-
         System.out.println("Player 1's Dominoes: " + game.getPlayer1().getHand().toString());
         System.out.println("Player 2's Dominoes: " + game.getPlayer2().getHand().toString());
-
         // Set up the Draw From Pile
         game.setBoneyard(new LinkedList<>(shuffledDominoes.subList(14,28)));
-
         System.out.println("Boneyard Dominoes: " + game.getBoneyard().toString());
-
         // Initialize the board and place the highest double domino
         // This logic also adjusts the players' hands and sets the score
         initializeBoardWithHighestDouble(game);
@@ -45,27 +40,19 @@ public class GameService {
         if (player != game.getCurrentPlayer()) {
             throw new IllegalArgumentException("It's not this player's turn");
         }
-
-        //If
-
         // 2. Validate if the move is legal
         List<Domino> playableDominoes = computePlayableDominoes(game);  // Assuming you've a method to compute playable dominoes
-
 
         if (!playableDominoes.contains(domino)) {
             // Player will have to draw from the boneyard instead of throwing this exception
             throw new IllegalArgumentException("Invalid move");
         }
-
-
         // 3. Update the board with the new domino
         game.getDominoBoard().addToBottomTrain(domino);
         player.getHand().remove(domino);
-
         // 4. Compute scores if necessary
         int newScore = computeScore(game); // Assuming you've a method to compute score based on the board state
         player.setScore(player.getScore() + newScore);
-
         // 5. Switch players for the next turn
         if (game.getCurrentPlayer() == game.getPlayer1()) {
             game.setCurrentPlayer(game.getPlayer2());
@@ -87,8 +74,6 @@ public class GameService {
     }
 
 
-
-
     public Game checkGameOver(Game game) {
         // Check for game over condition
         if(game.getPlayer1().getScore() >= 100 || game.getPlayer2().getScore() >= 100) {
@@ -101,22 +86,17 @@ public class GameService {
     private List<Domino> shuffleDominoes() {
         //list of all dominoes
         List<Domino> dominoes = new LinkedList<>();
-
         //add dominoes to dominoes list
         for(int i = 0; i <= 6; i++) {
             for(int j = 0; j <= i; j++) {
                 dominoes.add(new Domino(i, j));
             }
         }
-        //shuffle them up a little bit
         Collections.shuffle(dominoes);
-        //voila
         return dominoes;
     }
 
-
     private void initializeBoardWithHighestDouble(Game game) {
-
         // Find both the highest double domino and the player that has it - could return null
         PlayerDominoPair playerDominoPair = getHighestDoubleFromBothHands(
                 game.getPlayer1(),
@@ -124,7 +104,6 @@ public class GameService {
                 game.getPlayer2(),
                 (LinkedList<Domino>) game.getPlayer2().getHand()
         );
-
         // If there are no doubles found in either players hands
         if (playerDominoPair == null)
             // Set the current player to be player1
@@ -135,8 +114,6 @@ public class GameService {
             Domino highestDouble = playerDominoPair.getDomino();
             // Keep a record of the central piece on the domino board
             game.getDominoBoard().setCentralDomino(highestDouble);
-
-
         }
 
 
@@ -163,13 +140,10 @@ public class GameService {
     public PlayerDominoPair getHighestDoubleFromBothHands(Player player1, LinkedList<Domino> hand1, Player player2, LinkedList<Domino> hand2) {
         Domino highestDoubleHand1 = computeHighestDouble(hand1);
         Domino highestDoubleHand2 = computeHighestDouble(hand2);
-
         if(highestDoubleHand1 != null)
             System.out.println("Highest Double in hand 1: " + highestDoubleHand1);
         if(highestDoubleHand2 != null)
             System.out.println("Highest Double in hand 2: " + highestDoubleHand2);
-
-
         // Now, we'll compare the highest doubles from both hands to get the overall highest.
         if (highestDoubleHand1 == null && highestDoubleHand2 == null) {
             return null; // no doubles in both hands
